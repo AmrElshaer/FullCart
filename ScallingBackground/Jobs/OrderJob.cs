f4ab98351fb015cde298684288d44e71136a7ed2
amr elshaer <amr.elshaer@luminsoft.net>
2024-08-18 17:24:17 +0200

    init

---
+++ ScallingBackground/Jobs/OrderJob.cs
@@ -0,0 +1,67 @@
+﻿using Application.Common.Interfaces;
+using Domain.Orders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScallingBackground.Jobs;
+
+public interface IOrderJob
+{
+    Task UpdateOrderStatus();
+}
+
+public class OrderJob : IOrderJob
+{
+    private readonly ICartDbContext _cartDbContext;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<OrderJob> _logger;
+
+    public OrderJob(ICartDbContext cartDbContext,IConfiguration configuration,ILogger<OrderJob> logger)
+    {
+        _cartDbContext = cartDbContext;
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public async Task UpdateOrderStatus()
+    {
+        try
+        {
+            var serviceName = _configuration.GetValue<string>("Service:Name");
+            _logger.LogInformation($"Update Orders Background job service {serviceName}");
+            var orders =  _cartDbContext.Orders.Where(o => o.Status == OrderStatus.Pending)
+                .ToList();
+
+            foreach (var order in orders)
+            {
+                order.ChangeOrderStatus(OrderStatus.Confirmed);
+            }
+
+            _cartDbContext.SaveChangesAsync(default).GetAwaiter().GetResult();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+
+            throw;
+        }
+    }
+    
+}
+public class OrderJobWrapper
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public OrderJobWrapper(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public void UpdateOrderStatus()
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var orderJob = scope.ServiceProvider.GetRequiredService<IOrderJob>();
+            orderJob.UpdateOrderStatus();
+        }
+    }
+}
---
+++ ScallingBackground/Program.cs
@@ -0,0 +1,105 @@
+using System.Text;
+using System.Threading.RateLimiting;
+using Application;
+using Hangfire;
+using Infrastructure;
+using Infrastructure.Common.Persistence;
+using Infrastructure.Hubs.OrderHub;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.OpenApi.Models;
+using ScallingBackground.Jobs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddRateLimiter(l =>
+{
+    l.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    l.AddFixedWindowLimiter(policyName: "fixed", options =>
+        {
+            options.PermitLimit = 4;
+            options.Window = TimeSpan.FromSeconds(12);
+            options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+            options.QueueLimit = 2;
+        });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+
+builder.Services.AddSignalR();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition(name: "Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });
+
+    // Define the security requirement
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                },
+            },
+            new string[]
+                { }
+        },
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IOrderJob, OrderJob>();
+builder.Services.AddSingleton<OrderJobWrapper>();
+builder.Services.AddInfrastructure(builder.Configuration)
+    .AddApplication();
+builder.Services.AddHangfire(config =>
+    config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseDefaultTypeSerializer()
+        .UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+app.UseRateLimiter();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    await app.InitialiseDatabaseAsync();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHangfireDashboard();
+RecurringJob.AddOrUpdate<OrderJobWrapper>(
+    "UpdateOrderStatus",
+    job => job.UpdateOrderStatus(),
+    Cron.Minutely);
+app.UseCors("AllowSpecificOrigin");
+app.MapControllers();
+app.MapHub<OrderStatusHub>("/orderStatusHub");
+app.Run();
